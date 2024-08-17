d701d1f9075b2fe5e583b6f936968c35015ee78e
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-08-17 22:02:19 +0100

    pinnacle_landingpage

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ function Header() {
       <div className="client-review-container">
           <div className="flex justify-center">
             <div class="mt-[8rem] flex justify-center md:flex-row flex-col ">
-              <h2 className="text-[20px] lg:text-[1.7rem] font-bold text-center md:mt-[6rem] text-slate-300 font-mono  lg:[30rem] md:w-[30rem] sm:w-[20rem] w-[16rem]">EXPERIENCE OUR WINNING ONLINE TRADING PLATFORM
+              <h2 className="text-[20px] lg:text-[1.7rem] font-bold text-center md:mt-[6rem] text-slate-300 font-mono  lg:[30rem] md:w-[30rem] sm:w-[20rem] w-[16rem]">EXPERIENCE OUR WINNING ONLINE TRADING PLATFORM.<br/>
               We are excited to help you on your journey to Forex success
               </h2>
               <img src={img} className="lg:w-[30rem] md:w-[25rem] sm:w-[20rem] w-[16rem] rounded-[30px] mt-[3rem] md:mt-0" alt="img"/>
